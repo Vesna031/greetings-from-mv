b55e4b8336d5958026f2d8116395e89d8d84608d
David Rhoderick <david@artlyticalmedia.com>
2017-07-01 16:29:57 -0400

    Finishing project setup (GFMV-1) by setting up a gulp php server with file watching, bower dependency wiring, linting, and file minification.

--- dist/js/main-min.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){console.log("does this work?"),console.log("Hey vesna!  We are on your computer and we want to get out!!!")});
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,91 @@
-const gulp   = require('gulp');
-const jshint = require('gulp-jshint');
-const stylish = require('jshint-stylish');
-const minify = require('gulp-minify');
-const cleanCSS = require('gulp-clean-css');
-const concatCss = require('gulp-concat-css');
+var gulp   = require('gulp'),
+    wiredep = require('wiredep').stream,
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    minify = require('gulp-minify'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync'),
+    connect = require('gulp-connect-php');
+
+gulp.task('wiredep', function() {
+  gulp.src('index.html')
+    .pipe(wiredep({
+      overrides: {
+        'slick-lightbox': {
+          'main': 'dist/slick-lightbox.js'
+        }
+      }
+    }))
+    .pipe(gulp.dest('.'));
+
+  gulp.src('styles/main.css')
+    .pipe(wiredep({
+      overrides: {
+        'bootstrap': {
+          'main': 'dist/css/bootstrap.css'
+        }
+      },
+      fileTypes: {
+        css: {
+          block: /(([ \t]*)\/\*\s*bower:(\S*)\s*\*\/)(\n|\r|.)*?(\/\*\s*endbower\s*\*\/)/gi,
+          detect: {
+            css: /@import\s['"](.+css)['"];/gi
+          },
+          replace: {
+            css: '@import "{{filePath}}";'
+          }
+        }
+      }
+    }))
+    .pipe(gulp.dest('styles/'));
+})
 
 gulp.task('js', function() {
-  return gulp.src('./js/*.js')
+  gulp.src('js/main.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
     .pipe(minify({
-      noSource: true
+      ext: {
+        source: '.js',
+        min: '.min.js'
+      },
+      noSource: true,
     }))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('js/'));
 });
 
-gulp.task('styles', () => {
-  return gulp.src('styles/*.css')
-    .pipe(concatCss('dist/styles/main.css'))
+gulp.task('styles', function () {
+  gulp.src('styles/main.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/styles/main.css'));
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    .pipe(concat('main.min.css'))
+    .pipe(gulp.dest('styles/'));
 });
- 
-gulp.task('default', ['js', 'styles']);
+
+gulp.task('reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('watch', function() {
+  gulp.watch('bower.json', {ignoreInitial: false}, ['wiredep']);
+  gulp.watch('styles/main.css', {ignoreInitial: false}, ['styles', 'reload']);
+  gulp.watch('js/main.js', {ignoreInitial: false}, ['js', 'reload']);
+  gulp.watch('index.html', {ignoreInitial: false}, ['reload']);
+})
+
+gulp.task('default', ['wiredep', 'styles', 'js'], function() {
+  connect.server({
+    port: 3000
+  }, function () {
+    browserSync.init({
+      proxy: 'localhost:3000'
+    });
+  });
+
+  gulp.watch('styles/main.css', {ignoreInitial: false}, ['styles', 'reload']);
+  gulp.watch('js/main.js', {ignoreInitial: false}, ['js', 'reload']);
+  gulp.watch('index.html', {ignoreInitial: false}, ['reload']);
+});
