a4f83de4555ec3e8819d34bf2ae46189f3b61ab7
David Rhoderick <david@artlyticalmedia.com>
2020-07-13 15:39:50 +0200

    Fixing GFMV-79.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/itinerary">Itinerary</Link>
+            </li>
+            <li>
+              <Link to="/privacy-policy">Privacy Policy</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/itinerary">
+            <div>Itinerary</div>
+          </Route>
+          <Route path="/privacy-policy">
+            <div>Privacy Policy</div>
+          </Route>
+          <Route path="/">
+            <div>Home</div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
