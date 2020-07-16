e6f4681bf08ed0988e46445c981d370236010a6e
David Rhoderick <david@artlyticalmedia.com>
2020-07-16 19:53:19 +0200

    Done with GFMV-82.

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import Hero from './home/hero'
 import About from './home/about'
+import BookNow from './home/book-now'
 
 const Home = () => (
   <>
     <Hero />
-
     <About />
+    <BookNow />
   </>
 )
 
---
+++ src/pages/home/book-now.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+import Content from '../../components/content'
+
+import bookNowSprite from '../../images/buttons/book-now-sprite.gif'
+
+const BookNow = () => {
+  return(
+    <BookNowContent>
+      <BookNowButton onClick={() => console.log('Book Now!')} />
+    </BookNowContent>
+  )
+}
+
+export default BookNow
+
+const BookNowContent = styled(Content)`
+  display: flex;
+  justify-content: center;
+  padding: 0 1rem;
+`
+
+const BookNowButton = styled.button`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  width: 33%;
+  padding-bottom: 33%;
+
+  @media screen and (max-width: 768px) {
+    width: 50%;
+    padding-bottom: 50%;
+  }
+
+  background: url(${bookNowSprite});
+  background-size: 200% 100%;
+  background-repeat: no-repeat;
+  background-position: left center;
+  
+  &:hover {
+    background-position: right center;
+  }
+`
