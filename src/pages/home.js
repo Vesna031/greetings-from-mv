06d01af4cdf56cce8e32de4947d8a2b2387b8cd1
David Rhoderick <david@artlyticalmedia.com>
2020-07-16 21:01:02 +0200

    Started working on getting reviews but ran into CORS error.

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Hero from './home/hero'
 import About from './home/about'
 import BookNow from './home/book-now'
+import Reviews from './home/reviews'
 
 const Home = () => (
   <>
     <Hero />
     <About />
     <BookNow />
+    <Reviews />
   </>
 )
 
