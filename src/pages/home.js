4a71b32d029b2f812aefad19bf67bed33ae78738
David Rhoderick <david@artlyticalmedia.com>
2020-07-30 20:17:30 +0200

    Done adding photo gallery and fixing overflow on home page.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 import Hero from './home/hero'
 import About from './home/about'
 import BookNow from './home/book-now'
-import Reviews from './home/reviews'
+// import Reviews from './home/reviews'
+import Gallery from './home/gallery'
 
 const Home = () => (
-  <>
+  <HomeContainer>
     <Hero />
     <About />
     <BookNow />
-    <Reviews />
-  </>
+    {/* <Reviews /> */}
+    <Gallery />
+  </HomeContainer>
 )
 
 export default Home
+
+const HomeContainer = styled.div`
+  width: 100%;
+  overflow: hidden;
+`
