fc0db81b50acab683f11728d63c6f5e238a97280
David Rhoderick <david@artlyticalmedia.com>
2020-07-31 13:30:57 +0200

    Adding scroll to top functionality.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import ScrollToTop from 'react-scroll-to-top'
 
 import Header from '../components/header'
 import Hero from './home/hero'
@@ -10,8 +11,12 @@ import Gallery from './home/gallery'
 import Map from './home/map'
 import Instagram from './home/instagram'
 
+import scrollToTop from '../images/icons/scroll-to-top.png'
+
 const Home = () => (
   <>
+    <ScrollToTop smooth component={<ScrollToTopIcon />} style={{backgroundColor: 'transparent', boxShadow: 'none', width: '2rem', height: '2rem'}} />
+    
     <Header />
 
     <HomeContainer>
@@ -31,4 +36,15 @@ export default Home
 const HomeContainer = styled.div`
   width: 100%;
   overflow: hidden;
+`
+
+const ScrollToTopIcon = styled.div`
+  width: 3rem;
+  padding-bottom: 3rem;
+
+  background-image: url(${scrollToTop});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-size: contain;
+  background-position: center;
 `
