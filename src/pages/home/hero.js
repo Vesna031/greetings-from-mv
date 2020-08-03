0ae43cc055524689720109e00f994e2d51d85096
David Rhoderick <david@artlyticalmedia.com>
2020-08-03 15:29:42 +0200

    Fixing GFMV-95.

--- src/pages/home/hero.js
+++ src/pages/home/hero.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from '@emotion/styled'
+import window from 'global'
 
 import Content from '../../components/content'
 
@@ -7,15 +8,49 @@ import Hero1200 from '../../images/hero_sizes/hero_1200.png'
 import Hero992 from '../../images/hero_sizes/hero_992.png'
 import Hero768 from '../../images/hero_sizes/hero_768.png'
 
-const Hero = () => (
-  <div id="hero">
-    <HeroContent>
-      <div id="hero-content">
-        <img src={Hero768} srcSet={`${Hero992} 768w, ${Hero1200} 992w`} alt="Greetings From Martha's Vineyard" />
-      </div>
-    </HeroContent>
-  </div>
-)
+const Hero = () => {
+  const [heroImage, setHeroImage] = useState(Hero992)
+
+  const debounce = (fn, ms) => {
+    let timer
+    return _ => {
+      clearTimeout(timer)
+      timer = setTimeout(_ => {
+        timer = null
+        fn.apply(this)
+      }, ms)
+    }
+  }
+
+  useEffect(() => {
+    const debouncedHandleResize = debounce(
+      function handleResize() {
+        if(window.innerWidth <= 640) {
+          setHeroImage(Hero768)
+        } else if(window.innerWidth > 640 && window.innerWidth <= 992) {
+          setHeroImage(Hero992)
+        } else {
+          setHeroImage(Hero1200)
+        }
+      }, 250)
+
+    window.addEventListener('resize', debouncedHandleResize)
+
+    return _ => {
+      window.removeEventListener('resize', debouncedHandleResize)
+    }
+  })
+
+  return (
+    <div id="hero">
+      <HeroContent>
+        <div id="hero-content">
+          <img src={heroImage} alt="Greetings From Martha's Vineyard" />
+        </div>
+      </HeroContent>
+    </div>
+  )
+}
 
 export default Hero
 
